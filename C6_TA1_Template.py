60233715e385a0d5c2c7e65d0a068490cb4585f0
whjr2021 <91585088+whjr2021@users.noreply.github.com>
2021-10-31 11:32:56 +0530

    Add files via upload

---
+++ C6_TA1_Template.py
@@ -0,0 +1,13 @@
+# TA 1a: Create a Python list with elements 12, 56, 90 and name it as "roll_no".
+
+
+
+# TA 1b: Append the elements 67 and 39 to the "roll_no" list.
+
+
+
+
+
+# TA 1c: Remove the element 90 from "roll_no" list.
+
+
